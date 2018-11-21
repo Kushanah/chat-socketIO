881a69fd55710ec010534ee046f1e3188e3a570d
Kushanah <jeminemonblog@gmail.com>
2018-11-21 15:36:24 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 var io = require('socket.io')(http);
-const port = 8080;
+const port = process.env.PORT || 8080;
 var usersAlive = 0;
 
 app.use(express.static(__dirname + '/node_modules/socket.io-client/dist'));
